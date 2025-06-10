30fc5b320319198e44c1c664cca491eebae7225d
ryan <ryang@waabi.ai>
2025-06-10 16:04:04 +0000

    first commit

---
+++ examples/compile_opt/hello_comp_opt/src/main.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_main_runs() {
+        log::info!(
+            "Worker {} extracting polylines for map {}, processing {} geometries of {} total geometries",
+            1,
+            2,
+        );
+    }
+}
